5c4c3eb278874e7b7775c10af2bc11638126c98c
Scott Schulte <scott.r.schulte@gmail.com>
2024-06-17 20:30:39 -0500

    Vendors rendering on page.

--- src/components/Vendors/VendorList/VendorList.jsx
+++ src/components/Vendors/VendorList/VendorList.jsx
@@ -1,18 +1,25 @@
 import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 export default function VendorList() {
   const vendors = useSelector((store) => store.vendors);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_VENDORS'});
-  }, [])
+    dispatch({ type: 'FETCH_VENDORS' });
+  }, []);
 
   return (
     <>
-    <h1>Manage Vendors</h1>
+      <h1>Manage Vendors</h1>
+      <ul>
+        {vendors.map((vendor, i) => {
+          return <li key={i}>{vendor.vendor_name}</li>
+        })}
+      </ul>
     </>
-  )
+  );
 }

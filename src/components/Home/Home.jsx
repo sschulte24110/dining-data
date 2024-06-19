f77fee49972248e765965767c20537e09f6717f0
Scott Schulte <scott.r.schulte@gmail.com>
2024-06-19 15:05:24 -0500

    Update and add icons and links to navigate app.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,34 @@
+import { useHistory } from 'react-router-dom';
+import './Home.css';
+
 export default function Home() {
+  const history = useHistory();
+
   return (
-    <>
-      <h1>Dining Data</h1>
-    </>
+    <div>
+      <img
+        className='home-logo'
+        src='../images/dining-data-vert.png'
+        alt='Dining Data logo'
+      />
+      <br />
+      <br />
+      <div onClick={() => history.push('/beerstyle')}>
+        <img src='../images/icons8-beer-mug-99.png' alt='Beer Mug Icon' />
+        <h2>Beer</h2>
+      </div>
+      <div onClick={() => history.push('/wines')}>
+        <img src='../images/icons8-wine-glass-100 (1).png' alt='Wine Glass Icon' />
+        <h2>Wine</h2>
+      </div>
+      <div onClick={() => history.push('/vendors')}>
+        <img src='../images/icons8-delivery-truck-100.png' alt='Delivery Truck Icon' />
+        <h2>Vendors</h2>
+      </div>
+      <div>
+      <img src="../images/icons8-user-96.png" alt="User Icon" />
+        <h2>Users</h2>
+      </div>
+    </div>
   );
 }

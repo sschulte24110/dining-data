f9dffd65b0275b8475de5c327977fd4835091524
Scott Schulte <scott.r.schulte@gmail.com>
2024-06-21 14:49:09 -0500

    Add Beer Search to App.

--- src/components/Beers/BeerStyle/BeerStyle.jsx
+++ src/components/Beers/BeerStyle/BeerStyle.jsx
@@ -3,9 +3,6 @@ import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import './BeerStyle.css';
 import BeerStyleList from '../BeerStyleList/BeerStyleList';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Autocomplete from '@mui/material/Autocomplete';
 import BeerSearch from '../BeerSearch/BeerSearch';
 
 export default function BeerStyle() {
@@ -29,26 +26,7 @@ export default function BeerStyle() {
           <h3>Beer Styles</h3>
           <h6 onClick={() => history.push('/beerform')}>Add</h6>
         </div>
-        <div className='search-form'>
-          <Stack spacing={2} sx={{ width: 300 }}>
-            <Autocomplete
-              freeSolo
-              id='free-solo-2-demo'
-              disableClearable
-              options={beers.map((option) => option.brewery)}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label='Search beer by brewery'
-                  InputProps={{
-                    ...params.InputProps,
-                    type: 'search',
-                  }}
-                />
-              )}
-            />
-          </Stack>
-        </div>
+        <BeerSearch />
         <ul className='beer-styles-list'>
           {styles.map((style) => (
             <BeerStyleList key={style.id} style={style} />
